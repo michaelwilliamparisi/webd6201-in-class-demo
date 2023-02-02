b30a3561089b290fbc1877efb85785bb2ffc4dff
Michael Parisi <michael_parisi@live.com>
2023-02-01 19:12:08 -0500

    commit

---
+++ ICE4/scripts/main.js
@@ -0,0 +1,38 @@
+(function () {
+    
+    function DisplayButton(){
+        let randomButton = document.getElementById("randomButton")
+        randomButton.addEventListener("click", function(){
+            location.href ="./projects.html"
+        })
+
+        let mainContent = document.getElementsByTagName("main")[0]
+        mainContent.setAttribute("class", "container")
+
+        console.log(mainContent)
+
+        let mainParagraph = document.createElement("p")
+        mainParagraph.setAttribute("id", "MainParagraph")
+
+        let firstString = "This is a main paragraph that we added through javascript"
+
+        mainParagraph.textContent = firstString
+
+        mainContent.appendChild(mainParagraph) // adds something after it was written.
+    }
+
+    function Start() {
+        console.log("App Started!")
+        
+        switch (document.title){
+            case "Home - WEBD6201 Demo":
+                DisplayButton()
+                break
+            case "Projects - Demo":
+                DisplayButton()
+                break
+        }
+    }
+
+    window.addEventListener("load", Start)
+})()
